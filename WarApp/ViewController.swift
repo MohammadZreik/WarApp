651cd334bb59881904337ec650bd353ca28a7fd5
Mohammad Zreik <guszremo@student.gu.se>
2018-05-11 23:17:34 +0200

    Update score label + Improvements

--- WarApp/ViewController.swift
+++ WarApp/ViewController.swift
@@ -10,11 +10,18 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    // Images and Labels Outlet
     
     @IBOutlet weak var LeftImg: UIImageView!
     @IBOutlet weak var RightImg: UIImageView!
+   
     @IBOutlet weak var PlayerLabel: UILabel!
     @IBOutlet weak var CpuLabel: UILabel!
+    
+    // Score Value
+    
+    var PlayerScore = 0
+    var CpuScore = 0
     
     
     override func viewDidLoad() {
@@ -29,15 +36,35 @@ class ViewController: UIViewController {
 
     @IBAction func DealTapped(_ sender: Any) {
         
+        // Generate random numbers
+        
         let LeftCard = arc4random_uniform(13) + 2
         let RightCard = arc4random_uniform(13) + 2
         
+        // Update card images
         
         LeftImg.image = UIImage(named: "card\(LeftCard)")
         RightImg.image = UIImage(named: "card\(RightCard)")
         
         
+        // Update score
+        
+        if LeftCard > RightCard {
+            
+            PlayerScore += 1
+            PlayerLabel.text = String(PlayerScore)
+            
+        }
+        
+        else if RightCard > LeftCard {
+            
+            CpuScore += 1
+            CpuLabel.text = String(CpuScore)
+            
+        }
+        
     }
+    
     
 }
 
